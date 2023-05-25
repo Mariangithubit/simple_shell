4f0fecd60d109c6104c9dd021b28a49da6ab3c9e
Mariangithubit <metsyb1@hotmail.com>
2023-05-24 17:06:44 -0700

    shell

--- p_putchar.c
+++ p_putchar.c
@@ -1,7 +1,45 @@
 #include "shell.h"
 
 /**
- * p_puts - input string
+ * _puts - input string
+ * @str: string
+ * Retun: 0
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _putchar - print the string
+ * @c: char
+ * Return: 0
+ */
+int _putchar(char c)
+{
+        static int i;
+        static char buf[WRITE_BUF_SIZE];
+
+        if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+        {
+                write(1, buf, i);
+                i = 0;
+        }
+        if (c != BUF-FlUSH)
+                buf[i++] = c;
+        return (1);
+}
+
+/**
+ * p_puts - print input string
  * @str: string
  * Return: 0
  */
